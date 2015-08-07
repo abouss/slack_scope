27233599d1ae5ae9ab2af637d9f0ee616f8fd570
Anfal Boussayoud <anfal.boussayoud@gmail.com>
2015-08-07 12:14:16 -0400

    added horoscope conversion

--- horoscope.rb
+++ horoscope.rb
@@ -1,12 +1,13 @@
 require 'sinatra'
 require 'sinatra/multi_route'
 require 'json'
-require_relative 'parse_horoscope'
+require './parse_horoscope'
 load 'HOROSCOPE_SIGNS'
 
+# HOROSCOPE_SIGNS = {aboussayoud: 'Sagittarius'}
 
 route :get, :post, '/' do
 	name = params[:user_name]
-	#return find_horoscope(HOROSCOPE_SIGNS[:name])
-	return {text: find_horoscope(HOROSCOPE_SIGNS[:name])}.to_json
+	#return find_horoscope(HOROSCOPE_SIGNS[name.to_sym])
+	return {text: find_horoscope(HOROSCOPE_SIGNS[name.to_sym])}.to_json
 end
--- parse_horoscope.rb
+++ parse_horoscope.rb
@@ -1,13 +1,11 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-   
 
-
-def find_horoscope(sign)  
-  signs = { Sagittarius: 9, Scorpio: 8}
-  page = Nokogiri::HTML(open("http://new.theastrologer.com/daily-horoscope"))   
-  return page.css('div[class="shareable-section"]')[signs[:Sagittarius.to_sym]].text
+def find_horoscope(sign)
+	signs = {Sagittarius: '9',Scorpio: '8'}
+    page = Nokogiri::HTML(open("http://new.theastrologer.com/daily-horoscope"))
+    return page.css('div[class="shareable-section"]')[signs[sign.to_sym].to_i].text
 end 
 
 
