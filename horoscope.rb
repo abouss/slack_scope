1e212be9ba5f5bc33d948d1c3a80bd065fada5ac
Anfal Boussayoud <anfal.boussayoud@gmail.com>
2015-08-07 11:04:39 -0400

    testing name -> horoscope sign

--- horoscope.rb
+++ horoscope.rb
@@ -1,11 +1,17 @@
 require 'sinatra'
-require 'json'
 require 'sinatra/multi_route'
-require 'parse_horoscope'
+require 'json'
+require_relative 'parse_horoscope'
+
+HOROSCOPE_SIGNS = {aboussayoud: 'Sagittarius'}
 
 
 route :get, :post, '/' do
 	#print params
-	return {text: find_horoscope(params[:user_name]}).to_json
+	return {text: find_horoscope(HOROSCOPE_SIGNS[:params[:user_name].to_sym])}.to_json
 
 end
+
+# name = "aboussayoud"
+
+# puts find_horoscope(HOROSCOPE_SIGNS[:name])
