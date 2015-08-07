3ecb996b0c381d3864e273d2e1c24cd04baea57f
Anfal Boussayoud <anfal.boussayoud@gmail.com>
2015-08-07 12:25:06 -0400

    testing require statements

--- horoscope.rb
+++ horoscope.rb
@@ -1,13 +1,13 @@
 require 'sinatra'
 require 'sinatra/multi_route'
 require 'json'
-require './parse_horoscope'
+require_relative 'parse_horoscope'
 load 'HOROSCOPE_SIGNS'
 
 # HOROSCOPE_SIGNS = {aboussayoud: 'Sagittarius'}
 
 route :get, :post, '/' do
 	name = params[:user_name]
-	return name
-	#return {text: find_horoscope(HOROSCOPE_SIGNS[name.to_sym])}.to_json
+	#return name
+	return {text: find_horoscope(HOROSCOPE_SIGNS[name.to_sym])}.to_json
 end
