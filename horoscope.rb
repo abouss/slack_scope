bd01fbd7bd81f5f887ae4c648f563d6acd7706ac
Anfal Boussayoud <anfal.boussayoud@gmail.com>
2015-08-07 16:00:45 -0400

    updated to allow generic usage

--- horoscope.rb
+++ horoscope.rb
@@ -4,14 +4,22 @@ require 'json'
 require './parse_horoscope'
 require './HOROSCOPE_SIGNS'
 
-# HOROSCOPE_SIGNS = {aboussayoud: 'Sagittarius'}
 
 route :get, :post, '/' do
-	name = params[:user_name]
-	horoscope = find_horoscope(HOROSCOPE_SIGNS[name.to_sym]).split("\n\n")
-	emoji = HOROSCOPE_EMOJIS[HOROSCOPE_SIGNS[name.to_sym].to_sym]
 
-	horoscope = emoji[1] + emoji[0] + horoscope[0].tr("\n", "") + emoji[0] + emoji[1] + "\n"  + horoscope[1]
+	sign_text = params[:text].sub("pp horoscope ", "")
+	
+	if sign_text != "me"
+		sign_text = sign_text.capitalize
+		horoscope = find_horoscope(sign_text).split("\n\n")
+		emoji = HOROSCOPE_EMOJIS[sign_text.to_sym]
+		horoscope = emoji[0] + horoscope[0].tr("\n", "") + emoji[1]+ "\n"  + horoscope[1]
+	else
+		name = params[:user_name]
+		horoscope = find_horoscope(HOROSCOPE_SIGNS[name.to_sym]).split("\n\n")
+		emoji = HOROSCOPE_EMOJIS[HOROSCOPE_SIGNS[name.to_sym].to_sym]
+		horoscope = emoji[0] + horoscope[0].tr("\n", "") + emoji[1]+ "\n"  + horoscope[1]
+	end
 
 	return {text: horoscope}.to_json
 end
