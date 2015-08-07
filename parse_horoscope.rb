ea5fa0b7a8d533e40e175f36c15353c204eace47
Anfal Boussayoud <anfal.boussayoud@gmail.com>
2015-08-07 14:41:20 -0400

    updated horoscope grabber for latest update

--- parse_horoscope.rb
+++ parse_horoscope.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 
 def find_horoscope(sign)
-	#signs = {Sagittarius: '9',Scorpio: '8', Capricorn: '10',Aquarius: '11', Pisces: '12'}
+
     page = Nokogiri::HTML(open("http://new.theastrologer.com/daily-horoscope"))
     #horoscope = page.css('div[class="shareable-section"]')[signs[sign.to_sym].to_i].text.split("\n\n") 
     call = 'div[sunsign="' + sign + '"]'
