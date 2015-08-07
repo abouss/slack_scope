02b5a91c89e2b0d0a5c93ea8f9ee4b61135876e1
Anfal Boussayoud <anfal.boussayoud@gmail.com>
2015-08-07 16:06:34 -0400

    debugging manual horoscope call

--- horoscope.rb
+++ horoscope.rb
@@ -13,12 +13,12 @@ route :get, :post, '/' do
 		sign_text = sign_text.capitalize
 		horoscope = find_horoscope(sign_text).split("\n\n")
 		emoji = HOROSCOPE_EMOJIS[sign_text.to_sym]
-		horoscope = emoji[0] + horoscope[0].tr("\n", "") + emoji[1]+ "\n"  + horoscope[1]
+		horoscope = emoji[1] +emoji[0] + horoscope[0].tr("\n", "") + emoji[0] + emoji[1]+ "\n"  + horoscope[1]
 	else
 		name = params[:user_name]
 		horoscope = find_horoscope(HOROSCOPE_SIGNS[name.to_sym]).split("\n\n")
 		emoji = HOROSCOPE_EMOJIS[HOROSCOPE_SIGNS[name.to_sym].to_sym]
-		horoscope = emoji[0] + horoscope[0].tr("\n", "") + emoji[1]+ "\n"  + horoscope[1]
+		horoscope = emoji[1] +emoji[0] + horoscope[0].tr("\n", "") + emoji[0] + emoji[1]+ "\n"  + horoscope[1]
 	end
 
 	return {text: horoscope}.to_json
