fe9b58537c81bf4cd43e23dd6a80e4c615fd9ab5
John Doe <yiwen@usc.edu>
2015-08-07 14:52:16 -0400

    emojis

--- HOROSCOPE_SIGNS.rb
+++ HOROSCOPE_SIGNS.rb
@@ -1,1 +1,14 @@
 HOROSCOPE_SIGNS = {aboussayoud: 'Sagittarius', jchen: 'Scorpio'}
+HOROSCOPE_EMOJIS = {
+Aries: 'ram',
+Taurus:":cow2:",
+Gemini: ":two_men_holding_hands:",
+Cancer: ":crab:",
+Leo: ":cat2:", Virgo: ":princess:",
+Libra:":hourglass_flowing_sand:",
+Scorpio:":dragon:",
+Sagittarius:":arrow_upper_right:",
+Capricorn:":goat:",
+Aquarius:":swimmer:",
+Pisces:":tropical_fish:"
+}
--- parse_horoscope.rb
+++ parse_horoscope.rb
@@ -1,17 +1,16 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require './HOROSCOPE_SIGNS'
+
 
 def find_horoscope(sign)
 
     page = Nokogiri::HTML(open("http://new.theastrologer.com/daily-horoscope"))
-    #horoscope = page.css('div[class="shareable-section"]')[signs[sign.to_sym].to_i].text.split("\n\n") 
+    #horoscope = page.css('div[class="shareable-section"]')[signs[sign.to_sym].to_i].text.split("\n\n")
+    emoji = HOROSCOPE_EMOJIS[sign.to_sym]
     call = 'div[sunsign="' + sign + '"]'
     horoscope = page.css(call)[1].text.split("\n\n")
-   
-    return ":arrow_upper_right:" + horoscope[0].tr("\n", "") + ":arrow_upper_right:"+ "\n"  + horoscope[1]
-end 
 
-
-
-
+    return emoji + horoscope[0].tr("\n", "") + emoji+ "\n"  + horoscope[1]
+end
