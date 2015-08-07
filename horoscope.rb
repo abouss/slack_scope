4db54f0e5a8ec170c84217c8266ae7d64e862553
Anfal Boussayoud <anfal.boussayoud@gmail.com>
2015-08-07 15:48:09 -0400

    reverted code to that without extra horoscope funcitonality

--- horoscope.rb
+++ horoscope.rb
@@ -7,19 +7,11 @@ require './HOROSCOPE_SIGNS'
 # HOROSCOPE_SIGNS = {aboussayoud: 'Sagittarius'}
 
 route :get, :post, '/' do
+	name = params[:user_name]
+	horoscope = find_horoscope(HOROSCOPE_SIGNS[name.to_sym]).split("\n\n")
+	emoji = HOROSCOPE_EMOJIS[HOROSCOPE_SIGNS[name.to_sym].to_sym]
 
-	sign_text = params[:text].sub("pp horoscope ", "")
-	
-	if sign_text != "me"
-		horoscope = find_horoscope(sign_text).split("\n\n")
-		emoji = HOROSCOPE_EMOJIS[sign_text.to_sym]
-		horoscope = emoji[0] + horoscope[0].tr("\n", "") + emoji[2]+ "\n"  + horoscope[1]
-	else
-		name = params[:user_name]
-		horoscope = find_horoscope(HOROSCOPE_SIGNS[name.to_sym]).split("\n\n")
-		emoji = HOROSCOPE_EMOJIS[HOROSCOPE_SIGNS[name.to_sym].to_sym]
-		horoscope = emoji[0] + horoscope[0].tr("\n", "") + emoji[2]+ "\n"  + horoscope[1]
-	end
+	horoscope = emoji[1] + emoji[0] + horoscope[0].tr("\n", "") + emoji[0] + emoji[1] + "\n"  + horoscope[1]
 
 	return {text: horoscope}.to_json
 end
