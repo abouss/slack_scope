aa13a3d6334c61d40bfd42c461d8b0bab125fdfe
John Doe <yiwen@usc.edu>
2015-08-07 15:40:54 -0400

    ds

--- horoscope.rb
+++ horoscope.rb
@@ -7,12 +7,16 @@ require './HOROSCOPE_SIGNS'
 # HOROSCOPE_SIGNS = {aboussayoud: 'Sagittarius'}
 
 route :get, :post, '/' do
-	name = params[:user_name]
-	horoscope = find_horoscope(HOROSCOPE_SIGNS[name.to_sym]).split("\n\n")
-	emoji = HOROSCOPE_EMOJIS[HOROSCOPE_SIGNS[name.to_sym].to_sym]
-	puts "debug"
-	puts emoji
-	horoscope = emoji + horoscope[0].tr("\n", "") + emoji+ "\n"  + horoscope[1]
-
+	sign_text = params[:text].sub("pp horoscope ", "")
+	if text_sign != "me"
+		horoscope = find_horoscope(sign_text).split("\n\n")
+		emoji = HOROSCOPE_EMOJIS[text_sign.to_sym]
+		horoscope = emoji[0] + horoscope[0].tr("\n", "") + emoji[2]+ "\n"  + horoscope[1]
+	else
+		name = params[:user_name]
+		horoscope = find_horoscope(HOROSCOPE_SIGNS[name.to_sym]).split("\n\n")
+		emoji = HOROSCOPE_EMOJIS[HOROSCOPE_SIGNS[name.to_sym].to_sym]
+		horoscope = emoji[0] + horoscope[0].tr("\n", "") + emoji[2]+ "\n"  + horoscope[1]
+	end
 	return {text: horoscope}.to_json
 end
