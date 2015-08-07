b8af2c857ea2fd95b5057912b7555c3db38d2938
Anfal Boussayoud <anfal.boussayoud@gmail.com>
2015-08-07 14:59:54 -0400

    cleaned up structure of horoscope code

--- horoscope.rb
+++ horoscope.rb
@@ -8,6 +8,9 @@ require './HOROSCOPE_SIGNS'
 
 route :get, :post, '/' do
 	name = params[:user_name]
-	
-	return {text: find_horoscope(HOROSCOPE_SIGNS[name.to_sym])}.to_json
+	horoscope = find_horoscope(HOROSCOPE_SIGNS[name.to_sym]).text.split("\n\n")
+	emoji = HOROSCOPE_EMOJIS[HOROSCOPE_SIGNS[name.to_sym].to_sym]
+	horoscope = emoji + horoscope[0].tr("\n", "") + emoji+ "\n"  + horoscope[1]
+
+	return {text: horoscope}.to_json
 end
