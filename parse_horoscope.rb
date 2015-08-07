4cfd6f01c84fb15a34c25294035aca8616bb08f3
Anfal Boussayoud <anfal.boussayoud@gmail.com>
2015-08-07 13:24:31 -0400

    updated horoscope formatting

--- parse_horoscope.rb
+++ parse_horoscope.rb
@@ -3,9 +3,10 @@ require 'nokogiri'
 require 'open-uri'
 
 def find_horoscope(sign)
-	signs = {Sagittarius: '9',Scorpio: '8'}
+	signs = {Sagittarius: '9',Scorpio: '8', Capricorn: '10',Aquarius: '11', Pisces: '12'}
     page = Nokogiri::HTML(open("http://new.theastrologer.com/daily-horoscope"))
-    return page.css('div[class="shareable-section"]')[signs[sign.to_sym].to_i].text
+    horoscope = page.css('div[class="shareable-section"]')[signs[sign.to_sym].to_i].text.split("\n\n")
+    return ":arrow_upper_right:" + horoscope[0] + ":arrow_upper_right:"+ "\n"  + horoscope[1]
 end 
 
 
